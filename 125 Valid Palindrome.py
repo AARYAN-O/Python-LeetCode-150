75fd2e1d83a4733e2740c9a5b38001530d74f040
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2025-03-30 10:17:08 +0530

    Update 125 Valid Palindrome.py

--- 125 Valid Palindrome.py
+++ 125 Valid Palindrome.py
@@ -43,3 +43,28 @@ class Solution:
 
 # Time complexity : O(n)
 # Space Complexity : O(n)
+
+# Method 2 : Optimal
+# Since we had to search something or compare the values in the same collection , its better to use two pointer approach
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left,right=0,len(s)-1
+
+        while left<right:
+            if not s[left].isalnum():
+                left+=1
+                continue
+            
+            if not s[right].isalnum():
+                right-=1
+                continue
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left+=1
+            right-=1
+
+        return True 
+        
